ee70746b929554a8b0a74c48ca47e7b2622bdc28
hamzaelbakk <elbak.hamza.2018@gmail.com>
2023-02-04 21:35:18 +0100

    push

--- app/views/admins/addCruise.php
+++ app/views/admins/addCruise.php
@@ -1,88 +1,88 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
 <div class="row mb-5">
-    <div class="col-md-6 mx-auto">
-      <div class="card card-body bg-light mt-5">
-        <h2>Add Cruise</h2>
-        <p>Enter The Cruise Details</p>
-        <form method="post" enctype="multipart/form-data">
+  <div class="col-md-6 mx-auto">
+    <div class="card card-body bg-light mt-5 bg-dark text-white">
+      <h2>Add Cruise</h2>
+      <p>Enter The Cruise Details</p>
+      <form method="post" enctype="multipart/form-data">
         <div>
-            <div class="form-group">
-              <label>Image: <sup>*</sup></label>
-              <input type="file" name="image" class="form-control form-control-lg" required>
-            </div>
-            <div class="form-group">
-              <label>Title: <sup>*</sup></label>
-              <input type="text" name="title" class="form-control form-control-lg" required>
-            </div>
+          <div class="form-group">
+            <label>Image: </label>
+            <input type="file" name="image" class="form-control form-control-lg" required>
+          </div>
+          <div class="form-group">
+            <label>Title: </label>
+            <input type="text" name="title" class="form-control form-control-lg" required>
+          </div>
 
-            <div lass="form-group">
-              <label>Ship: <sup>*</sup></label>
-              <select class="form-control form-control-lg" id="ship" name="ship">
+          <div lass="form-group">
+            <label>Ship:</label>
+            <select class="form-control form-control-lg" id="ship" name="ship">
               <?php
-                foreach ($data['ship'] as $key) {
+              foreach ($data['ship'] as $key) {
               ?>
                 <option value="<?= $key->idship ?>"><?= $key->name ?></option>
               <?php } ?>
-              </select>
-            </div>
-            
-            <div class="form-group">
-              <label>Departure Date:<sup>*</sup></label>
-              <input type="date" name="departuredate" class="form-control form-control-lg" required>
-            </div>
-            <!-- <div class="form-group">
+            </select>
+          </div>
+
+          <div class="form-group">
+            <label>Departure Date:</label>
+            <input type="date" name="departuredate" class="form-control form-control-lg" required>
+          </div>
+          <!-- <div class="form-group">
               <label>Departure Port: <sup>*</sup></label>
               <input type="text" name="departureport" class="form-control form-control-lg" required>
             </div> -->
 
 
-            <div lass="form-group">
-              <label>Departure Port: <sup>*</sup></label>
-              <select class="form-control form-control-lg" id="departureport" name="departureport">
+          <div lass="form-group">
+            <label>Departure Port: </label>
+            <select class="form-control form-control-lg" id="departureport" name="departureport">
               <?php
-                foreach ($data['product'] as $key) {
+              foreach ($data['product'] as $key) {
               ?>
                 <option value="<?= $key->idport ?>"><?= $key->name ?> | <?= $key->country ?></option>
               <?php } ?>
-              </select>
-            </div>
-            <div lass="form-group">
-              <label>Destination: <sup>*</sup></label>
-              <select class="form-control form-control-lg" id="destination" name="destination">
+            </select>
+          </div>
+          <div lass="form-group">
+            <label>Destination: </label>
+            <select class="form-control form-control-lg" id="destination" name="destination">
               <?php
-                foreach ($data['product'] as $key) {
+              foreach ($data['product'] as $key) {
               ?>
                 <option value="<?= $key->name ?> | <?= $key->country ?>"><?= $key->name ?> | <?= $key->country ?></option>
               <?php } ?>
-              </select>
-            </div>
-            <div class="form-group">
-              <label>Duration: <sup>*</sup></label>
-              <input type="text" name="duration" class="form-control form-control-lg" required>
-            </div>
-            <div lass="form-group">
-              <label>Itinerary: <sup>*</sup></label>
-              <select class="form-control form-control-lg" id="itinerary" name="itinerary[]" type="checkbox" multiple>
+            </select>
+          </div>
+          <div class="form-group">
+            <label>Duration: </label>
+            <input type="text" name="duration" class="form-control form-control-lg" required>
+          </div>
+          <div lass="form-group">
+            <label>Itinerary: </label>
+            <select class="form-control form-control-lg" id="itinerary" name="itinerary[]" type="checkbox" multiple>
               <?php
-                foreach ($data['product'] as $key) {
+              foreach ($data['product'] as $key) {
               ?>
                 <option value="<?= $key->name ?> | <?= $key->country ?>"><?= $key->name ?> | <?= $key->country ?></option>
               <?php } ?>
-              </select>
-            </div>
-
-
-
+            </select>
           </div>
-            <div class="row mt-2">
-              <div class="col">
-                <input type="submit" value="Submit" class="btn btn-primary btn-block">
-              </div>
+
+
+
+        </div>
+        <div class="row mt-2">
+          <div class="col">
+            <input type="submit" value="Submit" class="btn btn-primary btn-block">
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   </div>
+</div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
--- app/views/admins/addRoomType.php
+++ app/views/admins/addRoomType.php
@@ -1,33 +1,33 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
 <div class="row mb-5">
-    <div class="col-md-6 mx-auto">
-      <div class="card card-body bg-light mt-5">
-        <h2>Add Room Type</h2>
-        <p>Enter The Room Type</p>
-        <form method="post" enctype="multipart/form-data">
+  <div class="col-md-6 mx-auto">
+    <div class="card card-body bg-light mt-5 bg-dark text-white">
+      <h2>Add Room Type</h2>
+      <p>Enter The Room Type</p>
+      <form method="post" enctype="multipart/form-data">
         <div>
-            <div class="form-group">
-              <label>Room Type:<sup>*</sup></label>
-              <input type="text" name="type" class="form-control form-control-lg" required>
-            </div>
-            <div class="form-group">
-              <label>Capacity: <sup>*</sup></label>
-              <input type="number" name="capacity" class="form-control form-control-lg" required>
-            </div>
-            <div class="form-group">
-              <label>Price: <sup>*</sup></label>
-              <input type="number" name="price" class="form-control form-control-lg" required>
-            </div>
+          <div class="form-group">
+            <label>Room Type:</label>
+            <input type="text" name="type" class="form-control form-control-lg" required>
           </div>
-            <div class="row mt-2">
-              <div class="col">
-                <input type="submit" value="Submit" class="btn btn-primary btn-block">
-              </div>
+          <div class="form-group">
+            <label>Capacity:</label>
+            <input type="number" name="capacity" class="form-control form-control-lg" required>
           </div>
-        </form>
-      </div>
+          <div class="form-group">
+            <label>Price: </label>
+            <input type="number" name="price" class="form-control form-control-lg" required>
+          </div>
+        </div>
+        <div class="row mt-2">
+          <div class="col">
+            <input type="submit" value="Submit" class="btn btn-primary btn-block">
+          </div>
+        </div>
+      </form>
     </div>
   </div>
+</div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
